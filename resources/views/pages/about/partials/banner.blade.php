6117ae1626bbc06567f3f16fca5349e2f65ac898
mtuchi <mtuchidev@gmail.com>
2019-06-26 16:42:37 +0300

    feat: update contact us title

--- resources/views/pages/about/partials/banner.blade.php
+++ resources/views/pages/about/partials/banner.blade.php
@@ -1,7 +1,7 @@
 <section id="banner" class="banner" role="banner">
     <div class="wrap">
         <h2 class="banner-slogan">
-            Glad you found us
+            Let's grab a cup of coffee (or tea) and have a chat
         </h2>
         @include('pages.about.partials.banner-contact',['financialContacts' => true])
     </div>
