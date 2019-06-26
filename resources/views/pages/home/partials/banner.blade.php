53aeeac26343c5ecafe5cfad9c2d217a1bebfa41
mtuchi <mtuchidev@gmail.com>
2019-06-26 16:44:03 +0300

    feat: update landing page intro

--- resources/views/pages/home/partials/banner.blade.php
+++ resources/views/pages/home/partials/banner.blade.php
@@ -1,11 +1,11 @@
 <section id="banner" class="banner" role="banner">
     <div class="wrap">
         <h1 class="banner-slogan">
-            No frills,<br>
-            just proven expertise
+            Build,<br>
+            for simplicity
         </h1>
         <p class="banner-intro">
-            We craft websites &amp; web applications <br>using Laravel &amp; Vue
+            We craft SIMPLE &amp; VALUABLE <br> solutions to everday challenges
         </p>
     </div>
 </section>
