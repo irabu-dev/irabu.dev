2f3ee55bc9b3a42eaa7776e33a60aaddb87a9d4d
mtuchi <mtuchidev@gmail.com>
2019-10-22 13:24:03 +0300

    feat: update cta modal

--- resources/views/layout/partials/modal-match.blade.php
+++ resources/views/layout/partials/modal-match.blade.php
@@ -1,26 +1,41 @@
 <div id="match" class="modal" onclick="history.back()">
     <div class="mr-16 bg-white max-w-xl max-h-screen p-8 z-20 overflow-scroll shadow-lg | md:p-16 md:mx-8">
-        <h2 class="text-center title-xl">A good match?</h2>
+        <h2 class="text-center title-xl">A good fit?</h2>
 
         <div class="mt-16 markup grid gap-8 | md:grid-cols " style="--cols: 1fr 1fr">
-            <div class=text-green>
+            <div class=text-blue>
                 <h3 class=title-sm>What we do best</h3>
                 <ul class="mt-4 line-l">
-                    <li>All things Laravel</li>
-                    <li>Custom frontend components</li>
-                    <li>Building APIs</li>
-                    <li>Simplifying things</li>
-                    <li>Clean solutions</li>
-                    <li>Integrating services</li>
+                    <li>Consultation</li>
+                    <li>Lean design and development</li>
+                    <li>Quick MVP approach(Minimum Viable Product)</li>
+                    <li>Simple and clean solutions</li>
+                    <li>Modular design</li>
+                    <li>Iterate based on feedback</li>
+                    <li>Testing and deployment</li>
+                    <li>Production and scaling</li>
                 </ul>
             </div>
             <div class="text-grey">
-                <h3 class=title-sm>Not our cup of tea</h3>
+                <h3 class=title-sm>Let's grab a cup of tea</h3>
                 <ul class="mt-4 line-l">
-                    <li>Wordpress themes</li>
-                    <li>Cutting corners</li>
-                    <li>Free mockups to win a job</li>
-                    <li>'Just execute the briefing'</li>
+                    <li>
+                        We need to understand your project, so that we can design and develop a custom solution to address
+                        the challenges.
+                    </li>
+                    <br>
+                    <li>Let's get in touch.</li>
+                    <li><a href="mailto:info@irabu.co.tz">info@irabu.co.tz</a></li>
+                    <li><a href="#tel">(+255) 744 650 849</a></li>
+                    <li>
+                        <a class="link" href="https://goo.gl/maps/mxz4TfEfKQciCeog8"
+                            target="_blank" rel="noreferrer noopener">
+                            Our office HQ
+                            <span class="icon px-2 fill-pink group-hover:opacity-75 transition-fill transition-fast">
+                                {{ svg('icons/fas-map-marker-alt') }}
+                            </span>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
