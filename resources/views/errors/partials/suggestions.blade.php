1e79ad3803ada282eb816695fd1f58d09345fcc9
mtuchi <mtuchidev@gmail.com>
2019-10-24 22:11:21 +0300

    feat: update our 500 error brief

--- resources/views/errors/partials/suggestions.blade.php
+++ resources/views/errors/partials/suggestions.blade.php
@@ -6,9 +6,27 @@
                     A few <br>suggestions
                 </h2>
                 <ul class="text-2xl links-white links-underline bullets bullets-white">
-                    <li><span class="icon">{{ svg('icons/far-angle-right') }}</span> <a href="{{ route('home') }}">Homepage</a></li>
-                    <li><span class="icon">{{ svg('icons/far-angle-right') }}</span> <a href="{{ route('open-source.packages') }}">Open source packages</a></li>
-                    <li><span class="icon">{{ svg('icons/far-angle-right') }}</span> <a href="{{ route('about') }}">About us</a></li>
+                    <li>
+                        <span class="icon">{{ svg('icons/far-angle-right') }}</span>
+                        <a href="{{ route('home') }}">Homepage</a>
+                    </li>
+                    <li>
+                        <span class="icon">{{ svg('icons/far-angle-right') }}</span>
+                        <a class="link-white link-underline" href="{{ mailto(
+                            'Potential project brief',
+                            'Tell us as much as you can about
+                            - your online project
+                            - your planning
+                            - your budget
+                            - …
+                            
+                            Anything that helps us to start straightforward!'
+                            ) }}">Brief us</a>
+                    </li>
+                    <li>
+                        <span class="icon">{{ svg('icons/far-angle-right') }}</span>
+                        <a href="{{ route('about') }}">About us</a>
+                    </li>
                 </ul>
             </div>
         </div>
